201a7045fdf399a002e9bac02025e551b650b25c
fayaz2k <fayazali2k@gmail.com>
2017-03-04 11:15:24 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var a, b, c, d;
+a = "Hello ";
+b = "World";
+c = "Programming";
+
+window.alert(a+" guys this "+b+" is great for "+c);
+
+window.alert(a+" "+ b);
+
+window.alert('Hellow ' + b);
+
+
+
+
+
+
+
+
+
+
+
